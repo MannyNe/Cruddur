5396bc86df5cd0a9540364fa0fd6d3a28137bf2a
Amann Negussie <amannnegussie2@outlook.com>
2023-04-08 11:06:41 +0000

    🔧 fix(message): fix messageing in production
    - update app to have rollbar
    - add script to kill connections
    - update `db.py`
    - update path for `update_cognito_user_ids.py`
    -update path for `seed.py` in ddb

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -19,6 +19,6 @@ RUN curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dea
     && sudo apt update \
     && sudo apt install -y postgresql-client-13 libpq-dev
 
-# Install PostgreSQL Client into Gitpod
+# Install Session manager plugin
 RUN curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb" \
     && sudo dpkg -i session-manager-plugin.deb
--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,9 +31,9 @@ import logging
 from time import strftime
 
 # ROLLBAR
-# import rollbar
-# import rollbar.contrib.flask
-# from flask import got_request_exception
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 # TOKEN AUTHORIZING MIDDLEWARE
 from middleware.token_authorize import token_authorize
@@ -94,22 +94,22 @@ cors = CORS(
 )
 
 # ROLLBAR
-#rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-#@app.before_first_request
-#def init_rollbar():
-#    """init rollbar module"""
-#    rollbar.init(
-#        # access token
-#        rollbar_access_token,
-#        # environment name
-#        'production',
-#        # server root directory, makes tracebacks prettier
-#        root=os.path.dirname(os.path.realpath(__file__)),
-#        # flask already sets up logging
-#        allow_logging_basic_config=False)
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
 
     # send exceptions from `app` to rollbar, using flask's signal system.
-#    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 #@app.after_request
 #def after_request(response):
