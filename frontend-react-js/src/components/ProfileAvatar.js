6763e16e65fa6cda62e333557ea602538ef1681d
Amann Negussie <amannnegussie2@outlook.com>
2023-07-29 19:11:32 +0300

    ⭐ new: cors fixed and upload added

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,7 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://assets.realmanny.tech/avatars/data.jpg")`;
+  const backgroundImage = `url("https://assets.cruddur.com/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
