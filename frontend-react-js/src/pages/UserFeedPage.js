a7ec611ba867c5a34f88a66be0bda7928164c22c
Amann Negussie <amannnegussie2@outlook.com>
2023-07-31 19:28:56 +0300

    ⭐ new(RDS): connect DB and set up post confirmation

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -1,16 +1,15 @@
-import './UserFeedPage.css';
+import "./UserFeedPage.css";
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import DesktopNavigation  from 'components/DesktopNavigation';
-import DesktopSidebar     from 'components/DesktopSidebar';
-import ActivityFeed from 'components/ActivityFeed';
-import ActivityForm from 'components/ActivityForm';
-import ProfileHeading from 'components/ProfileHeading';
-import ProfileForm from 'components/ProfileForm';
+import DesktopNavigation from "components/DesktopNavigation";
+import DesktopSidebar from "components/DesktopSidebar";
+import ActivityFeed from "components/ActivityFeed";
+import ActivityForm from "components/ActivityForm";
+import ProfileHeading from "components/ProfileHeading";
+import ProfileForm from "components/ProfileForm";
 
-
-import {checkAuth, getAccessToken} from '../lib/CheckAuth';
+import { checkAuth, getAccessToken } from "../lib/CheckAuth";
 
 export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -24,48 +23,52 @@ export default function UserFeedPage() {
 
   const loadData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/@${params.handle}`
-      await getAccessToken()
-      const access_token = localStorage.getItem("access_token")
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/@${params.handle}`;
+      await getAccessToken();
+      const access_token = localStorage.getItem("access_token");
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: `Bearer ${access_token}`
+          Authorization: `Bearer ${access_token}`,
         },
-        method: "GET"
+        method: "GET",
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        console.log('setprofile',resJson.profile)
-        setProfile(resJson.profile)
-        setActivities(resJson.activities)
+        console.log("setprofile", resJson.profile);
+        setProfile(resJson.profile);
+        setActivities(resJson.activities);
       } else {
-        console.log(res)
+        console.log(res);
       }
     } catch (err) {
       console.log(err);
     }
   };
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
     loadData();
     checkAuth(setUser);
-  }, [])
+  }, []);
 
   return (
     <article>
-      <DesktopNavigation user={user} active={'profile'} setPopped={setPopped} />
-      <div className='content'>
-        <ActivityForm popped={popped} setActivities={setActivities} />
-        <ProfileForm 
-          profile={profile}
-          popped={poppedProfile} 
-          setPopped={setPoppedProfile} 
+      <DesktopNavigation user={user} active={"profile"} setPopped={setPopped} />
+      <div className="content">
+        <ActivityForm
+          user_handle={user}
+          popped={popped}
+          setActivities={setActivities}
         />
-        <div className='activity_feed'>
+        <ProfileForm
+          profile={profile}
+          popped={poppedProfile}
+          setPopped={setPoppedProfile}
+        />
+        <div className="activity_feed">
           <ProfileHeading setPopped={setPoppedProfile} profile={profile} />
           <ActivityFeed activities={activities} />
         </div>
